62bfd61e2e956c19ced63d4d86a473001b2e8f72
Junior <eecfredes@gmail.com>
2025-07-27 13:31:15 -0300

    :rocket:Users query, done

--- internal/store/users.go
+++ internal/store/users.go
@@ -9,7 +9,7 @@ type User struct {
 	ID        int64  `json:"id"`
 	Username  string `json:"username"`
 	Email     string `json:"email"`
-	Password  string `json:"password"`
+	Password  string `json:"-"`
 	CreatedAt string `json:"created_at"`
 }
 
@@ -17,6 +17,25 @@ type UsersStore struct {
 	db *sql.DB
 }
 
-func (s *UsersStore) Create(ctx context.Context) error {
+func (s *UsersStore) Create(ctx context.Context, user *User) error {
+	query := `
+		INSERT INTO users (username, password, email) VALUES($1, $2, $3) RETURNIG id,
+		created_at
+	`
+
+	err := s.db.QueryRowContext(
+		ctx,
+		query,
+		user.Username,
+		user.Password,
+		user.Email,
+	).Scan(
+		&user.ID,
+		&user.CreatedAt,
+	)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
