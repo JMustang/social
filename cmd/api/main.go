1f389ed524b00a31c442541ab026f2c0f01b66e1
Junior <eecfredes@gmail.com>
2025-07-28 23:17:05 -0300

    :pencil2:fixing typos

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,10 +12,10 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
 		db: dbConfig{
-			addr:         env.GetString("DB_ADDR", "postgres//user:adminpassword@localhost/social?sslmode=disable"),
+			addr:         env.GetString("DB_ADDR", "postgres://admin:secret@localhost:5433/social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
-			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
 	}
 
@@ -29,6 +29,9 @@ func main() {
 	if err != nil {
 		log.Panic(err)
 	}
+
+	defer db.Close()
+	log.Println("✅ database connection pool established!")
 
 	store := store.NewStorage(db)
 
